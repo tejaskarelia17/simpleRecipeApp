73c357a3b30797ef595e5b852fd953da28b8e9ea
Tejas Karelia <tejas.karelia17@gmail.com>
2017-04-10 16:58:34 -0400

    handling form submission.

---
+++ public/js/source/components/recipeList.js
@@ -0,0 +1,43 @@
+const RecipeList = React.createClass({
+    getInitialState: function () {
+        return {recipes: []};
+    },
+    componentDidMount: function () {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: (recipeList) => {
+                this.setState({recipes: recipeList});
+            },
+            error: (xhr, status, err) => {
+                console.error(this.props.url, status, err.toString());
+            }
+        });
+    },
+    render: function () {
+        let recipeList = this.state.recipes;
+        let recipes = recipeList.map((recipe) => {
+            return (
+                <Recipe
+                    key={recipe.id}
+                    title={recipe.title}
+                    description={recipe.description}
+                    id={recipe.id}
+                    steps={recipe.steps}
+                    ingredients={recipe.ingredients} />
+            );
+        });
+
+        return (
+            <div className="recipe">
+                {recipes}
+                <hr />
+                <RecipeForm list={this.state.recipes}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <RecipeList url="/recipes"/>, document.getElementById('content'));
